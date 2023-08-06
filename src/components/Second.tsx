79aa513b396e6f424331a4dfdfd754ebf109870e
AYG3 <gilbertayoku3@.com>
2023-08-06 02:19:49 +0100

    second

--- src/components/Second.tsx
+++ src/components/Second.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Second = () => {
   return (
-    <div>Second</div>
+    <div>
+      <header className=''>
+        <h1 className='flex font-bold text-4xl ring-1 justify-center'> Find Best Tours </h1>
+        <p></p>
+      </header>
+    </div>  
   )
 }
 
-export default Second
+export default Second;
