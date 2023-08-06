2fee563e047b78347d4898ef86851601685ee8dc
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-08-06 03:38:19 +0100

    nnn

--- pages/First.tsx
+++ pages/First.tsx
@@ -1,4 +1,5 @@
-import Navbar from '@/components/Navbar'
+import Navbar from './Navbar'
+import './style.css'
 import React from 'react'
 
 const First = () => {
--- src/components/First.tsx
+++ src/components/First.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Navbar'
+import Navbar from '../../pages/Navbar'
 
 const First = () => {
   return (
