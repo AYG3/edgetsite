79ab321325a56c4706b3ccf5c350d46489e28512
AYG3 <gilbertayoku3@.com>
2023-08-09 16:37:44 +0100

    e

--- pages/RecDestinations.tsx
+++ pages/RecDestinations.tsx
@@ -16,81 +16,74 @@ function RecDestinations() {
             <p className='font-bold text-3xl'>Recommended Destinations</p>
         </div>
 
-        {/* <div className=""> */}
+        <div className=' sm:grid sm:grid-flow-col sm:grid-rows-3 lg:grid-rows-2 sm:mx-12 sm:justify-center sm:gap-8'>
 
-          <div className=' sm:grid sm:grid-flow-col sm:grid-rows-3 lg:grid-rows-2 sm:mx-12 sm:justify-center sm:gap-8'>
+          {/* CARD */}
+          <div className='relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group'>
+            <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
+            <div className='absolute lg:hidden flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl group-hover:flex'>
+              <p className=''>27+ Place</p>
+              <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
+              <button className='relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill className="absolute top-[5.5px] right-0"/> </button>
+            </div>
+          </div>
 
-            {/* CARD */}
-            <div className='relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group'>
-              <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
-              <div className='absolute lg:hidden flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl group-hover:flex'>
-                <p className=''>27+ Place</p>
-                <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
-                <button className='relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill className="absolute top-[5.5px] right-0"/> </button>
-              </div>
+          {/* CARD */}
+          <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group'>
+            <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
+            <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex duration-200 transition '>
+              <p className=''>27+ Place</p>
+              <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
+              <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
             </div>
-
-            {/* CARD */}
-            <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group'>
-              <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
-              <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex duration-200 transition '>
-                <p className=''>27+ Place</p>
-                <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
-                <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
-              </div>
+          </div>
+          {/* CARD */}
+          <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
+            <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
+            <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
+              <p className=''>27+ Place</p>
+              <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
+              <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
             </div>
-            {/* CARD */}
-            <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
-              <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
-              <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
-                <p className=''>27+ Place</p>
-                <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
-                <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
-              </div>
+          </div>
+          {/* CARD */}
+          <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
+            <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
+            <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
+              <p className=''>27+ Place</p>
+              <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
+              <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
             </div>
-            {/* CARD */}
-            <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
-              <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
-              <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
-                <p className=''>27+ Place</p>
-                <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
-                <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
-              </div>
+          </div>
+          {/* CARD */}
+          <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
+            <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
+            <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
+              <p className=''>27+ Place</p>
+              <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
+              <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
             </div>
-            {/* CARD */}
-            <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
-              <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
-              <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
-                <p className=''>27+ Place</p>
-                <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
-                <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
-              </div>
+          </div>
+          {/* CARD */}
+          <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
+            <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
+            <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
+              <p className=''>27+ Place</p>
+              <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
+              <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
             </div>
-            {/* CARD */}
-            <div className=' relative mt-8 sm:h-64 sm:w-72 lg:w-72 xl:w-96 xl:h-80 group '>
-              <Image src={AUS} alt="Australia" className='rounded-2xl object-cover object-center w-full h-full'/>
-              <div className='absolute flex flex-col justify-center items-start bg-white bg-opacity-90 space-y-4 top-0 px-4 h-full w-full py-4 rounded-2xl lg:hidden group-hover:flex transtion duration-200 '>
-                <p className=''>27+ Place</p>
-                <p className='font-bold text-2xl hover:text-orange-500 duration-200 cursor-pointer'>Australia</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi saepe vel ea.</p>
-                <button className=' relative font-bold text-orange-500 pr-6 hover:translate-x-1 duration-150'>Explore <BsFillArrowRightCircleFill  className="absolute top-[5.5px] right-0"/> </button>
-              </div>
-            </div>
-
-
           </div>
 
 
+        </div>
 
 
-
-
-        {/* </div> */}
     </div>
   )
 }
